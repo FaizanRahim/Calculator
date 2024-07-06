c4948258d8bac2c018484e06fa6767042ebc788e
FAIZAN RAHIM <154126251+FaizanRahim@users.noreply.github.com>
2024-07-06 11:34:01 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,72 @@
+
+function Multiply(){
+    let x = document.getElementById('mult1').value;
+
+    let y = document.getElementById('mult2').value;
+    
+    x=Number(x);
+    y=Number(y);
+    
+    
+    let total = document.getElementById('total').innerHTML =(x*y);
+    
+}
+
+
+console.log();
+
+function Addition(){
+    let x = document.getElementById('mult1').value;
+
+    let y = document.getElementById('mult2').value;
+    
+    x=Number(x);
+    y=Number(y);
+    
+    
+    let total = document.getElementById('total').innerHTML =(x+y);
+}
+
+console.log();
+
+function Division(){
+    let x = document.getElementById('mult1').value;
+
+    let y = document.getElementById('mult2').value;
+    
+    x=Number(x);
+    y=Number(y);
+    
+    
+    let total = document.getElementById('total').innerHTML =(x/y);
+}
+
+console.log();
+
+function Modulus(){
+    let x = document.getElementById('mult1').value;
+
+    let y = document.getElementById('mult2').value;
+    
+    x=Number(x);
+    y=Number(y);
+    
+    
+    let total = document.getElementById('total').innerHTML =(x%y);
+}
+
+console.log();
+
+function Subtract(){
+    let x = document.getElementById('mult1').value;
+
+    let y = document.getElementById('mult2').value;
+    
+    x=Number(x);
+    y=Number(y);
+    
+    
+    let total = document.getElementById('total').innerHTML =(x-y);
+}
+
+console.log();
